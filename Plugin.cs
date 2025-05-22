a61046fd2bbe2197040e9cb9bcfe5c3e76eb3cbf
captnced <cedric.elsaesser1603@gmail.com>
2025-05-22 22:15:45 +0200

    Made compatible with IMHelper ModMenu

--- Plugin.cs
+++ Plugin.cs
@@ -3,6 +3,7 @@ using BepInEx;
 using BepInEx.Logging;
 using BepInEx.Unity.IL2CPP;
 using BulwarkStudios.GameSystems.Ui;
+using IMHelper;
 using UnityEngine;
 
 namespace ReenableEditor;
@@ -14,18 +15,41 @@ public class Plugin : BasePlugin
 {
     private const string Guid = "captnced.ReenableEditor";
     private const string Name = "ReenableEditor";
-    private const string Version = "1.0.0";
+    private const string Version = "1.1.0";
     internal new static ManualLogSource Log;
+    private static bool enabled;
 
     public override void Load()
     {
         Log = base.Log;
-        IMHelper.GameStateHelper.addSceneChangedToInGameListener(inGameListener);
+        GameStateHelper.addSceneChangedToInGameListener(inGameListener);
+        if (IL2CPPChainloader.Instance.Plugins.ContainsKey("captnced.IMHelper")) enabled = ModsMenu.isSelfEnabled();
+        if (!enabled)
+            Log.LogInfo("Disabled by IMHelper!");
+        else
+            init();
+    }
+
+    private static void init()
+    {
         Log.LogInfo("Loaded \"" + Name + "\" version " + Version + "!");
+    }
+
+    private static void disable()
+    {
+        Log.LogInfo("Unloaded \"" + Name + "\" version " + Version + "!");
+    }
+    
+    public static void enable(bool value)
+    {
+        enabled = value;
+        if (enabled) init();
+        else disable();
     }
 
     private static void inGameListener()
     {
+        if (!enabled) return;
         var editorButton = GameObject.Find("Canvas/1920x1080/Top/Settings Button/Editor");
         editorButton.gameObject.SetActive(true);
         editorButton.GetComponent<UiButton>().add_OnTriggered(new Action(delegate { buttonTriggered(); }));
